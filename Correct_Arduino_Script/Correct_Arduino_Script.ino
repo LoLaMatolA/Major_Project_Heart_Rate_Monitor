675a36cba81eb24d8fe47fc69b81b3e2d17fbd83
Lola <82228130+LoLaMatolA@users.noreply.github.com>
2024-05-22 13:52:54 +0530

    Uploaded Script

---
+++ Correct_Arduino_Script/Correct_Arduino_Script.ino
@@ -0,0 +1,52 @@
+
+
+
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+
+//  Variables
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 514;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               //For 3.3 v use a value around 338 and for 5v use a value of 514.
+                               //Works better at 5v. The S(signal) goes to A0 and the '-' goes to ground of arduino
+                               //Heart beat takes some time to stabilize
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+
+void setup() {   
+
+  Serial.begin(9600);          // For Serial Monitor
+
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+  }
+}
+
+
+
+void loop() {
+
+ 
+
+if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+
+ 
+ Serial.println(myBPM);                        // Print the value inside of myBPM. 
+}
+
+  delay(20);                    // considered best practice in a simple sketch.
+
+}
+
+  
